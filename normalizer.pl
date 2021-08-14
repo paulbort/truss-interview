0964693ca32ed86da9e3a7e62c925a0bbdd42475
Paul Bort <paulbort@gmail.com>
2021-08-14 18:06:04 -0400

    Initial read/write loop

---
+++ normalizer.pl
@@ -0,0 +1,45 @@
+#!/bin/env perl
+
+=head1 normalizer.pl
+
+ normalizer.pl < input.csv > output.csv
+
+CSV conversion utility for Truss interview process.
+
+=cut
+
+use Encode qw(decode encode);
+
+my $DEBUG = 1;
+
+sub debug {
+    my ( @msgs ) = @_;
+    return if not $DEBUG;
+    print STDERR "$_\n" foreach @msgs;
+    return;
+}
+
+my $raw_input = join "\n", <>;
+debug "STARTUP READ " . length( $raw_input ) . " characters;"
+my $raw_octets = decode('UTF-8', $raw_input, Encode::FB_DEFAULT);
+my $csv = encode('UTF-8', $raw_octets, Encode::FB_CROAK);
+debug "STARTUP CLEAN " . length( $csv ) . " characters";
+
+my ( $header_line, @data_lines ) = split /\n/, $csv;
+
+say $header_line;
+
+debug "STARTUP LINES " . @data_lines;
+say process_line $_ foreach @data_lines;
+
+sub process_line {
+    my ( $raw_line ) = @_;
+    debug "PROCESS IN: [$raW_line]";
+    my @fields = $raw_line =~ /(?:^|,)(?=[^"]|(")?)"?((?(1)[^"]*|[^,"]*))"?(?=,|$)/;
+    debug "PROCESS FIELDS IN: [" . join '][', @fields . ']';
+    ... process fields ...
+    debug "PROCESS FIELDS OUT: [" . join '][', @fields . ']';
+    my $result_line = join ',', @fields;
+    debug "PROCESS OUT: [$result_line]";
+    return $result_line;
+}
